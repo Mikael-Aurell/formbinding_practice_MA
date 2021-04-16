7e5ba090c296f92892d52e6f8d4c9903dbfb8ad2
Mikael Aurell <aurell.mikael@gmail.com>
2021-04-16 17:57:27 +0200

    added CustomerServiceImpl

--- src/main/java/se/lexicon/formbinding_practice/service/CustomerServiceImpl.java
+++ src/main/java/se/lexicon/formbinding_practice/service/CustomerServiceImpl.java
@@ -3,8 +3,10 @@ package se.lexicon.formbinding_practice.service;
 import org.springframework.stereotype.Service;
 import se.lexicon.formbinding_practice.converter.CustomerConverter;
 import se.lexicon.formbinding_practice.dto.CustomerDto;
+import se.lexicon.formbinding_practice.entity.Customer;
 import se.lexicon.formbinding_practice.repository.CustomerRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -23,16 +25,27 @@ public class CustomerServiceImpl implements CustomerService{
 
     @Override
     public CustomerDto saveOrUpdate(CustomerDto customerDto) {
-        return null;
+        if (customerDto == null) throw new IllegalArgumentException("CustomerDto is not valid.");
+        Customer convertDtoToModel = customerConverter.toModel(customerDto);
+        Customer savedObject = customerRepository.save(convertDtoToModel);
+
+        return customerConverter.toDTO(savedObject);
     }
 
     @Override
     public List<CustomerDto> getAll() {
-        return null;
+        Iterable<Customer> iterable = customerRepository.findAll();
+        List<Customer> customerList = new ArrayList<>();
+        iterable.iterator().forEachRemaining(customerList::add);
+
+        return new ArrayList<>(customerConverter.toDTOs(customerList));
     }
 
     @Override
     public CustomerDto findByCustomerId(String id) {
-        return null;
+        if (id == null) throw new IllegalArgumentException("Customer id can't be null");
+        Customer customer = customerRepository.findById(id).orElse(null);
+
+        return customerConverter.toDTO(customer);
     }
 }
